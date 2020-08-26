9f587ba46530afbdf3babcff97b84dc441e2eb0b
Daniel Nagai <daniel@shop-office.net>
2020-08-26 05:41:41 +0000

    Updates

---
+++ precache-manifest.bfead62924bb787a91fc310e9ffcb6a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381228023c070da4fb76a82fb40e84e4",
+    "url": "/lexreal25/TodoApp.git/index.html"
+  },
+  {
+    "revision": "be0b57bbe74e046721c3",
+    "url": "/lexreal25/TodoApp.git/static/css/main.b2845c96.chunk.css"
+  },
+  {
+    "revision": "8cb620173de546cd7e16",
+    "url": "/lexreal25/TodoApp.git/static/js/2.b731b154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lexreal25/TodoApp.git/static/js/2.b731b154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0b57bbe74e046721c3",
+    "url": "/lexreal25/TodoApp.git/static/js/main.2115de3e.chunk.js"
+  },
+  {
+    "revision": "c88c03e786afb63925ba",
+    "url": "/lexreal25/TodoApp.git/static/js/runtime-main.aabb3656.js"
+  }
+]);
